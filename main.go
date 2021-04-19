0903c0f3fbd45804baa25b40ac04106f565114e6
Nils Dralle <nils.91@googlemail.com>
2021-04-19 16:29:12 +0200

    write input data on output

--- main.go
+++ main.go
@@ -3,11 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	var args []string
-	args = os.Args[1:]
+	args := os.Args[1:]
 
-	fmt.Println(args)
+	n, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	memsize := upow(2, uint(n))
+	filename := args[1]
+
+	fmt.Println("Address size: " + fmt.Sprint(n) + " bits")
+	fmt.Println("Memory space: " + fmt.Sprint(memsize) + " bits")
+	fmt.Println("Input file: " + filename)
+}
+
+func upow(n uint, i uint) uint {
+	if i == 0 {
+		return 1
+	}
+	return upow(n, i-1) * n
 }

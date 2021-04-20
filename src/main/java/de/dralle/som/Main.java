46aad5f15c7bb7c89d6c17fb33caa129e14f6f2c
Nils Dralle <nils.91@googlemail.com>
2021-04-20 12:27:18 +0200

    output all args

--- src/main/java/de/dralle/som/Main.java
+++ src/main/java/de/dralle/som/Main.java
@@ -13,7 +13,10 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < args.length; i++) {
+			String string = args[i];
+			System.out.println("Argument["+i+"]: "+string);
+		}
 
 	}
 

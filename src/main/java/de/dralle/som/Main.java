b44aadbe0abce349c153e9c817d372f6cc88dc30
Nils Dralle <nils.91@googlemail.com>
2021-04-22 15:33:11 +0200

    run file in AB format

--- src/main/java/de/dralle/som/Main.java
+++ src/main/java/de/dralle/som/Main.java
@@ -5,6 +5,7 @@ package de.dralle.som;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -25,26 +26,52 @@ import org.apache.commons.cli.ParseException;
  */
 public class Main {
 
-	public static final String VERSION="0.0.2-SNAPSHOT";
+	public static final String VERSION = "0.0.2-SNAPSHOT";
+
 	/**
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
 		parseCli(args);
-		for (int i = 0; i < args.length; i++) {
-			String string = args[i];
-			System.out.println("Argument[" + i + "]: " + string);
+	}
+
+	private static void parseCli(String[] args) throws IOException {
+		Options options = setupCliOptions();
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = null;
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		String argN = args[0];
-		String filename = args[1];
-		int memAddressSize = Integer.parseInt(argN);
-		long memSpaceSize = (long) Math.pow(2, memAddressSize);
-		File f = new File(filename);
-		System.out.println("Mem Address Size: " + memAddressSize);
-		System.out.println("Mem Space Size (Bits): " + memSpaceSize);
-		System.out.println("File: " + filename);
-		System.out.println("File exist: " + f.exists());
+		if (cmd.hasOption("version")) {
+			System.out.println(VERSION);
+		}
+		if (cmd.hasOption("help")) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("som-java", options);
+		}
+		boolean verbose = false;
+		if (cmd.hasOption("verbose")) {
+			verbose = true;
+		}
+		String infile = null;
+		if (cmd.hasOption("infile")) {
+			infile = cmd.getOptionValue("infile");
+		}
+		if (cmd.hasOption("run")) {
+			runProgramFromFile(infile, verbose);
+		}
+	}
+
+	private static void runProgramFromFile(String inputFile, boolean verbose) throws IOException {
+		File f = new File(inputFile);
+		if (verbose) {
+			System.out.println("File: " + f);
+			System.out.println("File exist: " + f.exists());
+		}
 		List<Boolean> bits = new ArrayList<>();
 		BufferedReader br = new BufferedReader(new FileReader(f));
 		int r = 0;
@@ -61,13 +88,21 @@ public class Main {
 			}
 		}
 		br.close();
+		int n = getAsUnsignedInt(bits.subList(1, 6))+4;
+		long memSpaceSize = (long) Math.pow(2, n);
+		if (verbose) {
+			System.out.println("Mem Address Size: " + n);
+			System.out.println("Mem Space Size (Bits): " + memSpaceSize);
+		}
 		int opcodeSize = 2;
-		int commandSize = opcodeSize + memAddressSize;
-		System.out.println("Opcode Size: " + opcodeSize);
-		System.out.println("Command Size: " + commandSize);
+		int commandSize = opcodeSize + n;
+		if (verbose) {
+			System.out.println("Opcode Size: " + opcodeSize);
+			System.out.println("Command Size: " + commandSize);
+		}
 		Boolean[] bitArray = bits.toArray(new Boolean[bits.size()]);
 		int ACC = 0;// accumulator address
-		int PC = 0;// program counter
+		int PC = 6;// program counter
 		int step = 0;
 		while (true) {
 			if (PC > bitArray.length - 1) {
@@ -75,7 +110,7 @@ public class Main {
 			}
 			Boolean[] commandBits = Arrays.copyOfRange(bitArray, PC, PC + commandSize);
 			Boolean[] opcodeBits = Arrays.copyOfRange(commandBits, 0, opcodeSize);
-			Boolean[] addressBits = Arrays.copyOfRange(commandBits, opcodeSize, opcodeSize + memAddressSize);
+			Boolean[] addressBits = Arrays.copyOfRange(commandBits, opcodeSize, opcodeSize + n);
 			Opcode op = null;
 			int address = 0;
 			if (!opcodeBits[0] && !opcodeBits[1]) {// 00
@@ -87,16 +122,14 @@ public class Main {
 			} else if (opcodeBits[0] && opcodeBits[1]) {// 11
 				op = Opcode.CJMP;
 			}
-			for (int i = 0; i < addressBits.length; i++) {
-				if (addressBits[i]) {
-					address += Math.pow(2, memAddressSize - i - 1);
-				}
+			address = getAsUnsignedInt(addressBits);
+			if (verbose) {
+				System.out.println("Accumulator: " + bitArray[ACC]);
+				System.out.println("Step: " + step);
+				System.out.println("PC: " + PC);
+				System.out.println("Opcode: " + op);
+				System.out.println("Address: " + address);
 			}
-			System.out.println("Accumulator: " + bitArray[ACC]);
-			System.out.println("Step: " + step);
-			System.out.println("PC: " + PC);
-			System.out.println("Opcode: " + op);
-			System.out.println("Address: " + address);
 			step++;
 			PC += commandSize;
 			switch (op) {
@@ -123,49 +156,39 @@ public class Main {
 		}
 		for (int i = 0; i < bitArray.length; i++) {
 			Boolean bit = bitArray[i];
-			if(bit) {
+			if (bit) {
 				System.out.print("1");
-			}else {
+			} else {
 				System.out.print("0");
 			}
-			
 		}
 	}
 
-	private static void parseCli(String[] args) {
-		Options options= setupCliOptions();
-		CommandLineParser parser = new DefaultParser();
-		CommandLine cmd = null;
-		try {
-			cmd = parser.parse(options, args);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	private static int getAsUnsignedInt(List<Boolean> subList) {
+		return getAsUnsignedInt(subList.toArray(new Boolean[subList.size()]));
+	}
+
+	private static int getAsUnsignedInt(Boolean[] array) {
+		int n = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i]) {
+				n += Math.pow(2, array.length - i - 1);
+			}
 		}
-		if(cmd.hasOption("version")) {
-			System.out.println(VERSION);
-		}
-		if(cmd.hasOption("help")) {
-			HelpFormatter formatter=new HelpFormatter();
-			formatter.printHelp("som-java", options);
-		}		
-		if(cmd.hasOption("verbose")) {
-			System.out.println("verbose");
-		}
-		System.exit(1);
+		return n;
 	}
 
 	private static Options setupCliOptions() {
-		Options options=new Options();
+		Options options = new Options();
 		options.addOption("v", "version", false, "Display the version");
 		options.addOption("h", "help", false, "Display all available CLI options");
-		options.addOption(null,"verbose",false,"Enable verbose mode");
-		options.addOption("in","infile",true,"Specify input file");
-		options.addOption("out","outfile",true,"Spacify output file");
-		options.addOption("if","informat",true,"Input file format");
-		options.addOption("of","outformat",true,"Output file format");
-		options.addOption(null,"run",false,"Run a file");
-		options.addOption(null,"compile",false,"Compile a file");
+		options.addOption(null, "verbose", false, "Enable verbose mode");
+		options.addOption("in", "infile", true, "Specify input file");
+		options.addOption("out", "outfile", true, "Spacify output file");
+		options.addOption("if", "informat", true, "Input file format");
+		options.addOption("of", "outformat", true, "Output file format");
+		options.addOption(null, "run", false, "Run a file");
+		options.addOption(null, "compile", false, "Compile a file");
 		return options;
 	}
 }

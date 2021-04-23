7cb3813a23d4c0a59d13922009394886b88a30a0
Nils Dralle <nils.91@googlemail.com>
2021-04-23 16:08:13 +0200

    first tests for bitcode runner

---
+++ src/test/java/de/dralle/som/test/BitcodeRunnerInitTest.java
@@ -0,0 +1,48 @@
+package de.dralle.som.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import de.dralle.som.SOMBitcodeRunner;
+
+class BitcodeRunnerInitTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	@Test
+	void testInitMemspace() {
+		SOMBitcodeRunner runner = new SOMBitcodeRunner(5, 11);
+		assertNotNull(runner);
+	}
+	@Test
+	void testInitMemspaceSize() {
+		SOMBitcodeRunner runner = new SOMBitcodeRunner(5, 11);
+		int expectedMemspaceSize=(int) Math.pow(2, 5);
+		int actualSize = runner.getMemSpace().length*8;
+		assertEquals(expectedMemspaceSize, actualSize);
+	}
+	@Test
+	void testInitMemspaceContent() {
+		SOMBitcodeRunner runner = new SOMBitcodeRunner(5, 11);
+		byte[] expected=new byte[] {0x05,0x60,0x00,0x00};
+		assertEquals(expected, runner.getMemSpace());
+	}
+}

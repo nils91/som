dd47b0952ab2be5a3975d422adcc3b09dc3c60fd
Nils Dralle <nils.91@googlemail.com>
2021-04-23 12:13:10 +0200

    implement set bit methods

---
+++ src/main/java/de/dralle/som/SOMBitcodeRunner.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package de.dralle.som;
+
+/**
+ * @author Nils Dralle
+ *
+ */
+public class SOMBitcodeRunner {
+	public SOMBitcodeRunner(int n) {
+		long bitCnt=(long) Math.pow(2, n);
+		long byteCnt=bitCnt/8;
+		byte[] memSpace = new byte[(int) byteCnt];
+		setBitsUnsigned(1,6,n,memSpace);
+	}
+
+	public static byte[] setBitsUnsigned(int lowerBound, int upperBound, int value, byte[] memSpace) {
+		int bits=upperBound-lowerBound;
+		for (int i = 0; i < bits; i++) {
+			boolean bitValue=value%(Math.pow(2, bits-i-1))==0?false:true;
+			memSpace=setBit(lowerBound+i,bitValue,memSpace);
+		}
+		return memSpace;
+	}
+
+	public static byte[] setBit(int address, boolean bitValue, byte[] memSpace) {
+		int byteAddress=address/8;
+		int offset=address%8;
+		byte bite=memSpace[byteAddress];
+		byte bitmask=(byte) (1<<offset);
+		if(!bitValue) {
+			bitmask=(byte) ~bitmask;
+			bite=(byte) (bite&bitmask);
+		}else {
+			bite=(byte) (bite|bitmask);
+		}
+		memSpace[byteAddress]=bite;
+		return memSpace;
+	}
+}

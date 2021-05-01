3ba7152985ed1509e67b1f9ab73977e5f9cf7b71
Nils Dralle <nils.91@googlemail.com>
2021-05-01 13:44:13 +0200

    first sample write hook

---
+++ src/test/java/de/dralle/som/test/util/TestWriteHook.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package de.dralle.som.test.util;
+
+import de.dralle.som.SOMBitcodeRunner;
+import de.dralle.som.writehooks.AbstractWriteHook;
+
+/**
+ * @author Nils Dralle
+ *
+ */
+public class TestWriteHook extends AbstractWriteHook {
+	private int writeTrgCnt = 0;
+	private int readTrgCnt = 0;
+
+	public int getWriteTrgCnt() {
+		return writeTrgCnt;
+	}
+
+	public int getReadTrgCnt() {
+		return readTrgCnt;
+	}
+
+	@Override
+	public boolean read(SOMBitcodeRunner runner) {
+		readTrgCnt++;
+		return false;
+	}
+
+	@Override
+	public boolean write(boolean accumutlatorValue, SOMBitcodeRunner runner) {
+		writeTrgCnt++;
+		return false;
+	}
+
+}

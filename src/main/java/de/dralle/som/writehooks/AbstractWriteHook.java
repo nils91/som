db96610bc6c3fbdba1de06e7d350536adb63f98e
Nils Dralle <nils.91@googlemail.com>
2021-05-01 12:05:56 +0200

    write hook base classes

---
+++ src/main/java/de/dralle/som/writehooks/AbstractWriteHook.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package de.dralle.som.writehooks;
+
+import de.dralle.som.SOMBitcodeRunner;
+
+/**
+ * @author Nils Dralle
+ *
+ */
+public class AbstractWriteHook implements IWriteHook{
+
+	@Override
+	public boolean read(SOMBitcodeRunner runner) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean write(SOMBitcodeRunner runner) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/de/dralle/som/writehooks/IWriteHook.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package de.dralle.som.writehooks;
+
+import de.dralle.som.SOMBitcodeRunner;
+
+/**
+ * @author Nils Dralle
+ *
+ */
+public interface IWriteHook {
+	public boolean read(SOMBitcodeRunner runner);
+	public boolean write(SOMBitcodeRunner runner);
+}

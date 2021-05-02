0225251c327743a8b584c055cbef305de76fa437
Nils Dralle <nils.91@googlemail.com>
2021-05-02 12:55:26 +0200

    opcode tests

--- src/test/java/de/dralle/som/test/BitcodeRunnerExecuteTests.java
+++ src/test/java/de/dralle/som/test/BitcodeRunnerExecuteTests.java
@@ -66,4 +66,78 @@ class BitcodeRunnerExecuteTests {
 		SOMBitcodeRunner runner=new SOMBitcodeRunner(entireFile);
 		assertFalse(runner.execute());
 	}
+	@Test
+	void testOpcodeREAD() throws IOException {
+		File f=new File("test/fixtures/ab/test_read.ab");
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		String entireFile="";
+		String nextLine=null;
+		do {
+			nextLine=br.readLine();
+			if(nextLine!=null) {
+				entireFile+=nextLine;
+			}			
+		}while(nextLine!=null);
+		br.close();
+		SOMBitcodeRunner runner=new SOMBitcodeRunner(entireFile);
+		assertTrue(runner.execute());
+		//should have written accumulator to 1
+		assertTrue(runner.getBit(0));
+	}
+	@Test
+	void testOpcodeWRITE() throws IOException {
+		File f=new File("test/fixtures/ab/test_write.ab");
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		String entireFile="";
+		String nextLine=null;
+		do {
+			nextLine=br.readLine();
+			if(nextLine!=null) {
+				entireFile+=nextLine;
+			}			
+		}while(nextLine!=null);
+		br.close();
+		SOMBitcodeRunner runner=new SOMBitcodeRunner(entireFile);
+		assertTrue(runner.execute());
+		//should have written bit 7 to 1
+		assertTrue(runner.getBit(7));
+	}
+	@Test
+	void testOpcodeNAND() throws IOException {
+		File f=new File("test/fixtures/ab/test_nand.ab");
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		String entireFile="";
+		String nextLine=null;
+		do {
+			nextLine=br.readLine();
+			if(nextLine!=null) {
+				entireFile+=nextLine;
+			}			
+		}while(nextLine!=null);
+		br.close();		
+		SOMBitcodeRunner runner=new SOMBitcodeRunner(entireFile);
+		boolean accBefore = runner.getBit(0);
+		assertTrue(runner.execute());
+		boolean accAfter = runner.getBit(0);
+		//the NAND in that particular test should have inverted the accumulator
+		assertNotEquals(accBefore, accAfter);
+	}
+	@Test
+	void testOpcodeCJMP() throws IOException {
+		File f=new File("test/fixtures/ab/test_cjmp.ab");
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		String entireFile="";
+		String nextLine=null;
+		do {
+			nextLine=br.readLine();
+			if(nextLine!=null) {
+				entireFile+=nextLine;
+			}			
+		}while(nextLine!=null);
+		br.close();
+		SOMBitcodeRunner runner=new SOMBitcodeRunner(entireFile);
+		assertTrue(runner.execute());
+		//if CJMP worked, it would´ve jumped over the command that set accumulator to 0
+		assertTrue(runner.getBit(0));
+	}
 }

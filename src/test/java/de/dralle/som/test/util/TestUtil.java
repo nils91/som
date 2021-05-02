7fe6444414bbc3ba59800f2cf7ce7b7117a5b3fc
Nils Dralle <nils.91@googlemail.com>
2021-05-02 15:30:23 +0200

    new test util

---
+++ src/test/java/de/dralle/som/test/util/TestUtil.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package de.dralle.som.test.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author Nils Dralle
+ *
+ */
+public class TestUtil {
+	public static String readFileToString(String path) throws IOException {
+		File f=new File(path);
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		String entireFile="";
+		String nextLine=null;
+		do {
+			nextLine=br.readLine();
+			if(nextLine!=null) {
+				entireFile+=nextLine;
+			}			
+		}while(nextLine!=null);
+		br.close();
+		return entireFile;
+	}
+}

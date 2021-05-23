2e66226bb1b020f532fdd0d5a0921e8e8b0a8a1d
Nils Dralle <nils.91@googlemail.com>
2021-05-23 15:24:40 +0200

    initial tests for boolean array memspace

---
+++ src/test/java/de/dralle/som/test/BooleanArrayMemspaceTests.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package de.dralle.som.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import de.dralle.som.BooleanArrayMemspace;
+
+/**
+ * @author Nils Dralle
+ *
+ */
+class BooleanArrayMemspaceTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSweepSet0() {
+		BooleanArrayMemspace memSpace = new BooleanArrayMemspace(65535);
+		for (int i = 0; i < 65535; i++) {
+			memSpace.setBit(i, false);
+		}
+		for (int i = 0; i < 65535; i++) {
+			assertFalse(memSpace.getBit(i));
+		}
+	}
+	@Test
+	void testSweepSet1() {
+		BooleanArrayMemspace memSpace = new BooleanArrayMemspace(65535);
+		for (int i = 0; i < 65535; i++) {
+			memSpace.setBit(i, true);
+		}
+		for (int i = 0; i < 65535; i++) {
+			assertTrue(memSpace.getBit(i));
+		}
+	}
+	@Test
+	void testSweepSetAlternating() {
+		BooleanArrayMemspace memSpace = new BooleanArrayMemspace(65535);
+		for (int i = 0; i < 65535; i++) {
+			memSpace.setBit(i, i%2==0);
+		}
+		for (int i = 0; i < 65535; i++) {
+			assertEquals(i%2==0,memSpace.getBit(i));
+		}
+	}
+}

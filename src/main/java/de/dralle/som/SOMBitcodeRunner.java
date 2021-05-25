7bc6bd0dcaa602bb9e4188eae9db7d7eff83e7ce
Nils Dralle <nils.91@googlemail.com>
2021-05-25 14:43:33 +0200

    refactor bitcode runner

--- src/main/java/de/dralle/som/SOMBitcodeRunner.java
+++ src/main/java/de/dralle/som/SOMBitcodeRunner.java
@@ -13,12 +13,14 @@ import de.dralle.som.writehooks.IWriteHook;
  *
  */
 public class SOMBitcodeRunner {
-	private static final int ACC_ADDRESS = 0;
-	private static final int START_ADDRESS_START = 9;
-	private static final int ADDRESS_SIZE_END = 7;
-	private static final int ADDRESS_SIZE_START = 1;
-	private static final int ADR_EVAL_ADDRESS = 8;
-	private byte[] memSpace;
+	private ISomMemspace memspace;
+	public ISomMemspace getMemspace() {
+		return memspace;
+	}
+
+	public void setMemspace(ISomMemspace memspace) {
+		this.memspace = memspace;
+	}
 	private List<IWriteHook> writeHooks;
 	private int selectedWriteHook = 0;
 	private boolean lastWriteHookSwitchSuccess;
@@ -37,166 +39,58 @@ public class SOMBitcodeRunner {
 		return null;
 	}
 
-	public byte[] getMemSpace() {
-		return memSpace;
-	}
-
-	public static int getWriteHookEnabledAddress(byte[] memSpace) {
-		return ADR_EVAL_ADDRESS + getN(memSpace) + 1;
-	}
-
-	public static int getWriteHookDirectionAddress(byte[] memSpace) {
-		return getWriteHookEnabledAddress(memSpace) + 1;
-	}
-
-	public static int getWriteHookCommunicationAddress(byte[] memSpace) {
-		return getWriteHookEnabledAddress(memSpace) + 2;
-	}
-
-	public static int getWriteHookSelectAddress(byte[] memSpace) {
-		return getWriteHookEnabledAddress(memSpace) + 3;
-	}
-
-	public static boolean isWriteHookReadmode(byte[] memSpace) {
-		return !getBit(getWriteHookDirectionAddress(memSpace), memSpace);
-	}
-
-	public static boolean isWriteHookWritemode(byte[] memSpace) {
-		return getBit(getWriteHookDirectionAddress(memSpace), memSpace);
-
-	}
-
-	public static boolean isWriteHookSelected(byte[] memSpace) {
-		return getBit(getWriteHookSelectAddress(memSpace), memSpace);
-
-	}
-
-	public static boolean isWriteHookSwitchmodeSelected(byte[] memSpace) {
-		return !getBit(getWriteHookSelectAddress(memSpace), memSpace);
-	}
-
-	public int getWriteHookEnabledAddress() {
-		return getWriteHookEnabledAddress(memSpace);
-	}
-
-	public int getWriteHookDirectionAddress() {
-		return getWriteHookDirectionAddress(memSpace);
-	}
-
-	public int getWriteHookCommunicationAddress() {
-		return getWriteHookCommunicationAddress(memSpace);
-	}
-
-	public int getWriteHookSelectAddress() {
-		return getWriteHookSelectAddress(memSpace);
-	}
-
-	public boolean isWriteHookReadmode() {
-		return isWriteHookReadmode(memSpace);
-	}
-
-	public boolean isWriteHookWritemode() {
-		return isWriteHookWritemode(memSpace);
-	}
-
-	public boolean isWriteHookSelected() {
-		return isWriteHookSelected(memSpace);
-
-	}
-
-	public boolean isWriteHookSwitchmodeSelected() {
-		return isWriteHookSwitchmodeSelected(memSpace);
-	}
-
-	public static int getN(byte[] memSpace) {
-		int addressSizeBits = getBitsUnsignedBounds(ADDRESS_SIZE_START, ADDRESS_SIZE_END + 1, memSpace);
-		return addressSizeBits;
-	}
-
-	public int getN() {
-		return getN(memSpace);
-	}
-
-	/**
-	 * Address bits. If ADR_EVAL is set, these contain the next memory address, from
-	 * which execution will continue.
-	 * 
-	 * @return
-	 */
-	public static int getAddressBits(byte[] memSpace) {
-		int addressSizeBits = getN(memSpace);
-		int startAddress = getBitsUnsignedBounds(START_ADDRESS_START, START_ADDRESS_START + addressSizeBits, memSpace);
-		return startAddress;
-	}
-
-	/**
-	 * Address bits. If ADR_EVAL is set, these contain the next memory address, from
-	 * which execution will continue.
-	 * 
-	 * @return
-	 */
-	public int getAddressBits() {
-		return getAddressBits(memSpace);
-	}
+	
 
 	public SOMBitcodeRunner(int n, int startAddress) {
-		byte[] memSpace = initMemspaceFromAddressSizeAndStartAddress(n, startAddress);
-		this.memSpace = memSpace;
+		ISomMemspace memSpace = initMemspaceFromAddressSizeAndStartAddress(n, startAddress);
+		this.memspace = memSpace;
 	}
 
-	private byte[] initMemspaceFromAddressSizeAndStartAddress(int addressSizeBits, int startAddress) {
-		long bitCnt = (long) Math.pow(2, addressSizeBits);
-		long byteCnt = bitCnt / 8;
-		byte[] memSpace = new byte[(int) byteCnt];
+	private ISomMemspace initMemspaceFromAddressSizeAndStartAddress(int addressSizeBits, int startAddress) {
+		int bitCnt = (int) Math.pow(2, addressSizeBits);
+		ISomMemspace memSpace = new BooleanArrayMemspace(bitCnt);
 		memSpace = initEmptyMemspaceFromAddressSizeAndStartAddress(addressSizeBits, startAddress, memSpace);
 		return memSpace;
 	}
 
-	private byte[] initEmptyMemspaceFromAddressSizeAndStartAddress(int addressSizeBits, int startAddress,
-			byte[] memSpace) {
-		memSpace = setBitsUnsignedBounds(ADDRESS_SIZE_START, ADDRESS_SIZE_END + 1, addressSizeBits, memSpace);
-		memSpace = setBitsUnsignedBounds(START_ADDRESS_START, START_ADDRESS_START + addressSizeBits , startAddress,
-				memSpace);
+	private ISomMemspace initEmptyMemspaceFromAddressSizeAndStartAddress(int addressSizeBits, int startAddress,
+			ISomMemspace memSpace) {
+		memSpace.setN(addressSizeBits);
+		memSpace.setNextAddress(startAddress);
+		memSpace.setAdrEval();
 		return memSpace;
 	}
 
-	public SOMBitcodeRunner(byte[] memSpace) {
+	public SOMBitcodeRunner(ISomMemspace memSpace) {
 		memSpace = initFromExistingPartialMemspace(memSpace);
-		this.memSpace = memSpace;
+		this.memspace = memSpace;
 	}
 
-	private byte[] initFromExistingPartialMemspace(byte[] memSpace) {
-		int addressSizeBits = getN(memSpace);
-		int startAddress = getAddressBits(memSpace);
-		byte[] origgMemSpace = memSpace;
+	private ISomMemspace initFromExistingPartialMemspace(ISomMemspace memSpace) {
+		int addressSizeBits =memSpace.getN();
+		int startAddress = memspace.getNextAddress();
+		ISomMemspace origgMemSpace = memSpace.clone();
 		memSpace = initMemspaceFromAddressSizeAndStartAddress(addressSizeBits, startAddress);
-		for (int i = 0; i < origgMemSpace.length; i++) {
-			memSpace[i] = origgMemSpace[i];
-		}
+		memSpace.copy(origgMemSpace);
 		return memSpace;
 	}
 
 	public SOMBitcodeRunner(boolean[] bits) {
-		byte[] memSpace = initFromBooleanArray(bits);
-		this.memSpace = memSpace;
+		ISomMemspace memSpace = initFromBooleanArray(bits);
+		this.memspace = memSpace;
 	}
 
-	private byte[] initFromBooleanArray(boolean[] bits) {
-		int bitCnt = bits.length;
-		int byteCnt = bitCnt / 8;
-		if (bitCnt % 8 != 0) {
-			byteCnt++;
-		}
-		byte[] memSpace = new byte[(int) byteCnt];
+	private ISomMemspace initFromBooleanArray(boolean[] bits) {
+		ISomMemspace memSpace = new BooleanArrayMemspace(bits.length);
 		for (int i = 0; i < bits.length; i++) {
 			boolean b = bits[i];
-			memSpace = setBit(i, b, memSpace);
+			memSpace.setBit(i, b);
 		}
 		memSpace = initFromExistingPartialMemspace(memSpace);
 		return memSpace;
 	}
 
-	private byte[] initFromBooleanArray(Boolean[] bits) {
+	private ISomMemspace initFromBooleanArray(Boolean[] bits) {
 		boolean[] realBooleanArray = new boolean[bits.length];
 		for (int i = 0; i < realBooleanArray.length; i++) {
 			realBooleanArray[i] = bits[i];
@@ -205,8 +99,8 @@ public class SOMBitcodeRunner {
 	}
 
 	public SOMBitcodeRunner(List<Boolean> bits) {
-		byte[] memSpace = initFromBooleanArray(bits.toArray(new Boolean[bits.size()]));
-		this.memSpace = memSpace;
+		ISomMemspace memSpace = initFromBooleanArray(bits.toArray(new Boolean[bits.size()]));
+		this.memspace = memSpace;
 	}
 
 	public SOMBitcodeRunner(String bits) {
@@ -216,90 +110,17 @@ public class SOMBitcodeRunner {
 			boolean b = bits.charAt(i) != '0';
 			boolArr[i] = b;
 		}
-		this.memSpace = initFromBooleanArray(boolArr);
+		this.memspace = initFromBooleanArray(boolArr);
 	}
-
-	public static byte[] setBitsUnsignedBounds(int lowerBound, int upperBound, int value, byte[] memSpace) {
-		int bits = upperBound - lowerBound;
-		for (int i = 0; i < bits; i++) {
-			boolean bitValue = value - Math.pow(2, bits - i - 1) >= 0 ? true : false;
-			if (bitValue) {
-				value -= Math.pow(2, bits - i - 1);
-			}
-			memSpace = setBit(lowerBound + i, bitValue, memSpace);
-		}
-		return memSpace;
-	}
-
-	public static byte[] setBitsUnsigned(int lowerBound, int n, int value, byte[] memSpace) {
-		return setBitsUnsignedBounds(lowerBound, lowerBound + n, value, memSpace);
-	}
-
-	public static byte[] setBit(int address, boolean bitValue, byte[] memSpace) {
-		int byteAddress = address / 8;
-		int offset = 7 - address % 8;
-		byte bite = memSpace[byteAddress];
-		byte bitmask = (byte) (1 << offset);
-		if (!bitValue) {
-			bitmask = (byte) ~bitmask;
-			bite = (byte) (bite & bitmask);
-		} else {
-			bite = (byte) (bite | bitmask);
-		}
-		memSpace[byteAddress] = bite;
-		return memSpace;
-	}
-
-	public static boolean getBit(int address, byte[] memSpace) {
-		int byteAddress = address / 8;
-		int offset = 7 - address % 8;
-		byte bite = memSpace[byteAddress];
-		byte bitmask = (byte) (1 << offset);
-		boolean bitValue = (bite & bitmask) != 0;
-		return bitValue;
-	}
-
-	public static int getBitsUnsignedBounds(int lowerBound, int upperBound, byte[] memSpace) {
-		int bits = upperBound - lowerBound;
-		int returnValue = 0;
-		for (int i = 0; i < bits; i++) {
-			if (getBit(lowerBound + i, memSpace)) {
-				int bitmask = 1 << (bits - i - 1);
-				returnValue = returnValue | bitmask;
-			}
-		}
-		return returnValue;
-	}
-
-	public static int getBitsUnsigned(int lowerBound, int n, byte[] memSpace) {
-		return getBitsUnsignedBounds(lowerBound, lowerBound + n, memSpace);
-	}
-
-	public byte[] setBit(int address, boolean bitValue) {
-		return setBit(address, bitValue, memSpace);
-	}
-
-	public boolean getBit(int address) {
-		return getBit(address, memSpace);
-	}
-
-	public int getBitsUnsignedBounds(int lowerBound, int upperBound) {
-		return getBitsUnsignedBounds(lowerBound, upperBound, memSpace);
-	}
-
-	public int getBitsUnsigned(int lowerBound, int n) {
-		return getBitsUnsigned(lowerBound, n, memSpace);
-	}
-
 	public boolean execute() {
 		int programCounter = 0;
 		boolean accumulator = false;
 		int addressSize = 0;
 		do {
-			accumulator = getBit(ACC_ADDRESS);
-			addressSize = getN();
-			int startAddress = getAddressBits();
-			boolean addressEval = getBit(ADR_EVAL_ADDRESS);
+			accumulator = memspace.getAccumulatorValue();
+			addressSize = memspace.getN();
+			int startAddress = memspace.getNextAddress();
+			boolean addressEval = memspace.isAdrEvalSet();
 			int opcodeSize = 1;
 			int commandSize = addressSize + opcodeSize;
 			if (addressEval) {
@@ -308,7 +129,7 @@ public class SOMBitcodeRunner {
 			// get next command
 			boolean[] nextCommand = new boolean[commandSize];
 			for (int i = 0; i < nextCommand.length; i++) {
-				nextCommand[i] = getBit(programCounter + i);
+				nextCommand[i] = memspace.getBit(programCounter + i);
 			}
 			boolean opCode = nextCommand[0];
 			boolean[] tgtAddress = new boolean[addressSize];
@@ -316,19 +137,19 @@ public class SOMBitcodeRunner {
 				tgtAddress[i] = nextCommand[i + opcodeSize];
 			}
 			int tgtAddressValue = Util.getAsUnsignedInt(tgtAddress);
-			boolean tgtBitValue = getBit(tgtAddressValue);
+			boolean tgtBitValue = memspace.getBit(tgtAddressValue);
 			boolean nand = !(accumulator && tgtBitValue);
 			if (!opCode)// NAR
 			{
-				setBit(ACC_ADDRESS, nand);
+				memspace.setAccumulatorValue(nand);
 			} else {
-				setBit(tgtAddressValue, nand);
+				memspace.setBit(tgtAddressValue, nand);
 			}
 			// get write hook bits
-			boolean whEn = getBit(getWriteHookEnabledAddress());
-			boolean whDir = getBit(getWriteHookDirectionAddress());
-			boolean whCom = getBit(getWriteHookCommunicationAddress());
-			boolean whSel = getBit(getWriteHookSelectAddress());
+			boolean whEn = memspace.isWriteHookEnabled();
+			boolean whDir = memspace.isWriteHookWritemode();
+			boolean whCom = memspace.getWriteHookCommunicationBit();
+			boolean whSel = memspace.isWriteHookSelected();
 
 			if (whEn) {
 				// enabled
@@ -346,8 +167,8 @@ public class SOMBitcodeRunner {
 						if (currentlySelectedWriteHook != null) {
 							boolean hasNew = currentlySelectedWriteHook.hasDataAvailable();
 							boolean readBit = currentlySelectedWriteHook.read(this);
-							setBit(getWriteHookCommunicationAddress(), readBit);
-							setBit(getWriteHookDirectionAddress(), !hasNew);
+							memspace.setWriteHookCommunicationBit(readBit);
+							memspace.setWriteHookDirectionBit(!hasNew);
 						}
 					}
 				} else {
@@ -355,7 +176,7 @@ public class SOMBitcodeRunner {
 					if (whDir) {
 						// switch selected write hook
 						if (selectedWriteHook > 0 && selectedWriteHook < writeHooks.size() - 1) {
-							boolean comValue = getBit(getWriteHookCommunicationAddress());
+							boolean comValue = memspace.getWriteHookCommunicationBit();
 							if (comValue) {
 								selectedWriteHook++;
 							} else {
@@ -367,14 +188,14 @@ public class SOMBitcodeRunner {
 						}
 					} else {
 						// last switching success
-						setBit(getWriteHookCommunicationAddress(), lastWriteHookSwitchSuccess);
-						setBit(getWriteHookDirectionAddress(), false);
+						memspace.setWriteHookCommunicationBit(lastWriteHookSwitchSuccess);
+						memspace.setWriteHookDirectionBit(false);
 					}
 				}
 			}
 
 			programCounter += commandSize;
 		} while (programCounter < Math.pow(2, addressSize));
-		return getBit(ACC_ADDRESS);
+		return memspace.getAccumulatorValue();
 	}
 }

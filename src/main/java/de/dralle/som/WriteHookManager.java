6f380cdb278e4c2325202b2d85bcae0cc6016d3e
Nils Dralle <nils.91@googlemail.com>
2021-06-06 15:03:43 +0200

    write hook manager class

--- src/main/java/de/dralle/som/WriteHookManager.java
+++ src/main/java/de/dralle/som/WriteHookManager.java
@@ -23,4 +23,43 @@ public class WriteHookManager {
 	private int maxWhNumber=0;
 	private Map<Integer,IWriteHook> registeredWriteHooks=new HashMap<>();
 	private int selectedWriteHook=MIN_WH_NUMBER;
+	private boolean lastSwitchSuccess;
+	public boolean isLastSwitchSuccess() {
+		return lastSwitchSuccess;
+	}
+	public int getMaxWhNumber() {
+		return maxWhNumber;
+	}
+	public int getSelectedWriteHookNumber() {
+		return selectedWriteHook;
+	}
+	
+	public void setSelectedWriteHook(int selectedWriteHook) {
+		if(selectedWriteHook>=MIN_WH_NUMBER&&selectedWriteHook<=maxWhNumber) {
+			this.selectedWriteHook=selectedWriteHook;
+			lastSwitchSuccess=true;
+		}else {
+			lastSwitchSuccess=false;
+		}
+	}
+	public void switchToPreviousWriteHook() {
+		setSelectedWriteHook(getSelectedWriteHookNumber()-1);
+	}
+	public void switchToNextWriteHook() {
+		setSelectedWriteHook(getSelectedWriteHookNumber()+1);
+	}
+	public void registerWriteHook(int num,IWriteHook writeHook) {
+		if(num>maxWhNumber) {
+			maxWhNumber=num;
+		}
+		if(num>=MIN_WH_NUMBER) {
+			registeredWriteHooks.put(num, writeHook);
+		}
+	}
+	public void registerWriteHook(IWriteHook writeHook) {
+		registerWriteHook(selectedWriteHook+1, writeHook);
+	}
+	public IWriteHook getSelectedWriteHook() {
+		return registeredWriteHooks.get(selectedWriteHook);
+	}
 }

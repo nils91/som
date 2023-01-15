2011ba3a9b639273ecbeae1a10cbe4ef8a198766
Nils Dralle <nils.91@googlemail.com>
2023-01-15 19:34:43 +0100

    compilation methods

--- src/main/java/de/dralle/som/languages/hra/HRAModel.java
+++ src/main/java/de/dralle/som/languages/hra/HRAModel.java
@@ -3,12 +3,17 @@
  */
 package de.dralle.som.languages.hra;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.Callable;
 
 import de.dralle.som.AbstractSomMemspace;
+import de.dralle.som.ByteArrayMemspace;
+import de.dralle.som.IMemspace;
+import de.dralle.som.ISomMemspace;
 import de.dralle.som.SOMBitcodeRunner;
 import de.dralle.som.languages.hra.model.Command;
 
@@ -20,102 +25,197 @@ public class HRAModel {
 	public HRAModel() {
 		setupBuiltins();
 	}
+
 	private int n;
 	private int startAdress;
 	boolean startAddressExplicit;
+
 	public boolean isStartAddressExplicit() {
 		return startAddressExplicit;
 	}
+
 	public void setStartAddressExplicit(boolean startAddressExplicit) {
 		this.startAddressExplicit = startAddressExplicit;
 	}
+
 	public int getStartAddress() {
-		if(startAddressExplicit) {
+		if (startAddressExplicit) {
 			return startAdress;
 		}
-		return getFixedBitCount()+symbols.size();
+		startAdress = getFixedBitCount() + symbols.size();
+		return startAdress;
 	}
+
 	public int getHeapSize() {
-		int minHeap=(int) (Math.pow(2, n))-getFixedBitCount()-getCommandBits();
-		if(heap<minHeap) {
-			heap=minHeap;
+		int minHeap = (int) (Math.pow(2, n)) - getFixedBitCount() - getCommandBits();
+		if (heap < minHeap) {
+			heap = minHeap;
 		}
 		return heap;
 	}
+
 	private int heap;
-	private Map<String,Integer> symbols;
-	private Map<String,Integer> builtins;
-	private List<Command> commands;
+	private Map<String, Integer> symbols;
+	private Map<String, Integer> builtins;
+	private Map<Integer, Command> commands;
+
 	private void setupBuiltins() {
-		builtins=new HashMap<>();
-		builtins.put("ACC",AbstractSomMemspace.ACC_ADDRESS);
-		builtins.put("ADR_EVAL",AbstractSomMemspace.ADR_EVAL_ADDRESS);
-		builtins.put("WH_EN",AbstractSomMemspace.WH_EN);
+		builtins = new HashMap<>();
+		builtins.put("ACC", AbstractSomMemspace.ACC_ADDRESS);
+		builtins.put("ADR_EVAL", AbstractSomMemspace.ADR_EVAL_ADDRESS);
+		builtins.put("WH_EN", AbstractSomMemspace.WH_EN);
 		builtins.put("N", AbstractSomMemspace.ADDRESS_SIZE_START);
-		builtins.put("WH_COM",AbstractSomMemspace.WH_COM);
-		builtins.put("WH_DIR",AbstractSomMemspace.WH_DIR);
-		builtins.put("WH_SEL",AbstractSomMemspace.WH_SEL);
-		builtins.put("ADR",AbstractSomMemspace.START_ADDRESS_START);
+		builtins.put("WH_COM", AbstractSomMemspace.WH_COM);
+		builtins.put("WH_DIR", AbstractSomMemspace.WH_DIR);
+		builtins.put("WH_SEL", AbstractSomMemspace.WH_SEL);
+		builtins.put("ADR", AbstractSomMemspace.START_ADDRESS_START);
 	}
+
 	private int recalculateN() {
-		int minN=Integer.MAX_VALUE;
-		int minBitCount=getFixedBitCount()+symbols.size()+getCommandBits()+heap;
-		int i=0;
-		while(i<10000&&minBitCount>Math.pow(2, n)) {
+		int minN = Integer.MAX_VALUE;
+		int minBitCount = getFixedBitCount() + symbols.size() + getCommandBits() + heap;
+		int i = 0;
+		while (i < 10000 && minBitCount > Math.pow(2, n)) {
 			n++;
-			 minBitCount=getFixedBitCount()+symbols.size()+getCommandBits()+heap;
-			 if(minBitCount<getHighestTgtAddress()+1) {
-				 minBitCount=getHighestTgtAddress()+1;
-			 }
+			minBitCount = getFixedBitCount() + symbols.size() + getCommandBits() + heap;
+			if (minBitCount < getHighestTgtAddress() + 1) {
+				minBitCount = getHighestTgtAddress() + 1;
+			}
 		}
 		return n;
 	}
+
 	private boolean checkN() {
-		int minBitCount = getFixedBitCount()+symbols.size()+getCommandBits()+heap;
-		 if(minBitCount<getHighestTgtAddress()+1) {
-			 minBitCount=getHighestTgtAddress()+1;
-		 }
-		 return Math.pow(2, n) <=minBitCount;
+		int minBitCount = getFixedBitCount() + symbols.size() + getCommandBits() + heap;
+		if (minBitCount < getHighestTgtAddress() + 1) {
+			minBitCount = getHighestTgtAddress() + 1;
+		}
+		return Math.pow(2, n) <= minBitCount;
 	}
-	private int getHighestTgtAddress(){
-		int high=0;
+
+	private int getHighestTgtAddress() {
+		int high = 0;
 		for (Command command : commands) {
-			if(getCommandAddress(command)>high) {
-				getCommandAddress(command);
+			if (getCommandTargetAddress(command) > high) {
+				getCommandTargetAddress(command);
 			}
 		}
 		return high;
 	}
-	private int getCommandAddress(Command c) {
+
+	private int getCommandTargetAddress(Command c) {
 		int tgtAdddress = resolveSymbolToAddress(c.getTgtSymbol());
-		return tgtAdddress+c.getAddressOffset();
+		return tgtAdddress + c.getAddressOffset();
 	}
+
 	private int resolveSymbolToAddress(String symbol) {
 		Integer target = builtins.get(symbol);
-		if(target!=null) {
+		if (target != null) {
 			return target.intValue();
 		}
-		target=symbols.get(symbol);
-		if(target!=null) {
+		target = symbols.get(symbol);
+		if (target != null) {
 			return target.intValue();
 		}
-		target=Integer.parseInt(symbol);
-		if(target!=null) {
+		target = Integer.parseInt(symbol);
+		if (target != null) {
 			return target.intValue();
 		}
 		return 0;
 	}
+
 	private int getCommandBits() {
-		int commandSize=getCommandSize();
-		return commands.size()*commandSize;
+		int commandSize = getCommandSize();
+		return commands.size() * commandSize;
 	}
+
 	private int getCommandSize() {
-		return 1+n;
+		return 1 + n;
 	}
+
 	private int getFixedBitCount() {
-		return 11+n;
+		return 11 + n;
 	}
+
 	private int getExitAddress() {
-		return (int) (Math.pow(2, n)-getCommandSize());
+		return (int) (Math.pow(2, n) - getCommandSize());
+	}
+
+	private String getNDirective() {
+		return String.format(";n = %d", recalculateN());
+	}
+
+	private String getHeapDirective() {
+		return String.format(";heap = %d", getHeapSize());
+	}
+
+	private String getStartDirective() {
+		return String.format(";start = %d", getStartAddress());
+	}
+
+	private String getContinueDirective(int continueAddress) {
+		return String.format(";continue = %d", continueAddress);
+	}
+
+	private List<String> getSymbolsAsStrings() {
+		List<String> tmp = new ArrayList<>();
+		for (Entry<String, Integer> symbol : symbols.entrySet()) {
+			tmp.add(String.format("%s %d", symbol.getKey(), symbol.getValue().intValue()));
+		}
+		return tmp;
+	}
+
+	private List<String> getCommandssAsStrings() {
+		List<String> tmp = new ArrayList<>();
+		for (Entry<Integer, Command> c : commands.entrySet()) {
+			Integer address = c.getKey();
+			Command command = c.getValue();
+			tmp.add(String.format("%s%s%s %s[%d]", getContinueDirective(address.intValue()), System.lineSeparator(),
+					command.getOp(), command.getTgtSymbol(), command.getAddressOffset()));
+		}
+		return tmp;
+	}
+
+	public String asCode() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(getNDirective());
+		sb.append(System.lineSeparator());
+		sb.append(getHeapDirective());
+		sb.append(System.lineSeparator());
+		sb.append(getStartDirective());
+		sb.append(System.lineSeparator());
+		for (String symbolString : getSymbolsAsStrings()) {
+			sb.append(symbolString);
+			sb.append(System.lineSeparator());
+		}
+		for (String symbolString : getCommandssAsStrings()) {
+			sb.append(symbolString);
+			sb.append(System.lineSeparator());
+		}
+		return sb.toString();
+	}
+
+	public IMemspace compileToMemspace() {
+		ISomMemspace mem = new ByteArrayMemspace((int) Math.pow(2, recalculateN()));
+		mem.setN(recalculateN());
+		mem.setNextAddress(getStartAddress());
+		for (Entry<Integer, Command> c : commands.entrySet()) {
+			Integer address = c.getKey();
+			Command command = c.getValue();
+			switch (command.getOp()) {
+			case NAR:
+				mem.setBit(address.intValue(), false);
+				break;
+			case NAW:
+				mem.setBit(address.intValue(), true);
+				break;
+			default:
+				break;
+			}
+			mem.setBitsUnsignedBounds(address.intValue() + 1, recalculateN(), getCommandTargetAddress(command));
+		}
+		mem.setAccumulatorValue(true);
+		mem.setAdrEval(true);
+		return mem;
 	}
 }

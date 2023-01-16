be82627bfd87e151cb2ca1051b0765d2501f52fa
Nils Dralle <nils.91@googlemail.com>
2023-01-16 16:20:31 +0100

    parser class completed

--- src/main/java/de/dralle/som/languages/hra/HRAParser.java
+++ src/main/java/de/dralle/som/languages/hra/HRAParser.java
@@ -3,6 +3,7 @@
  */
 package de.dralle.som.languages.hra;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -12,15 +13,22 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import de.dralle.som.languages.hra.generated.HRAGrammarLexer;
 import de.dralle.som.languages.hra.generated.HRAGrammarParser;
 import de.dralle.som.languages.hra.generated.HRAGrammarParser.ProgramContext;
+import de.dralle.som.languages.hra.model.HRAModel;
+import de.dralle.som.languages.hra.visitors.ProgramVisitor;
 
 /**
  * @author Nils
  *
  */
 public class HRAParser {
-	public void parse(InputStream is) throws IOException {
+	public HRAModel parse(InputStream is) throws IOException {
 		HRAGrammarLexer lexer = new HRAGrammarLexer(new ANTLRInputStream(is));
 		HRAGrammarParser parser = new HRAGrammarParser(new CommonTokenStream(lexer));
 		ProgramContext pt = parser.program();
-	}	
+		HRAModel model = pt.accept(new ProgramVisitor());
+		return model;
+	}
+	public HRAModel parse(String s) throws IOException {
+		return parse(new ByteArrayInputStream(s.getBytes()));
+	}
 }

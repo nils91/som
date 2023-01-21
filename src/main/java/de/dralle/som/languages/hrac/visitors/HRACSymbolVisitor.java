61eecee306a8aa20de8aafbb59be7a1eb7852234
Nils Dralle <nils.91@googlemail.com>
2023-01-21 12:41:25 +0100

    visitors maybe done

---
+++ src/main/java/de/dralle/som/languages/hrac/visitors/HRACSymbolVisitor.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package de.dralle.som.languages.hrac.visitors;
+
+import de.dralle.som.Opcode;
+import de.dralle.som.languages.hrac.generated.HRACGrammarBaseVisitor;
+import de.dralle.som.languages.hrac.generated.HRACGrammarParser.Cnt_specifyContext;
+import de.dralle.som.languages.hrac.generated.HRACGrammarParser.Symbol_decContext;
+import de.dralle.som.languages.hrac.model.HRACCommand;
+import de.dralle.som.languages.hrac.model.HRACSymbol;
+import de.dralle.som.languages.hras.generated.HRASGrammarBaseVisitor;
+import de.dralle.som.languages.hras.generated.HRASGrammarParser.CommandContext;
+import de.dralle.som.languages.hras.model.Command;
+
+/**
+ * @author Nils
+ *
+ */
+public class HRACSymbolVisitor extends HRACGrammarBaseVisitor<HRACSymbol> {
+	private HRACSymbol s;
+
+	@Override
+	public HRACSymbol visitCnt_specify(Cnt_specifyContext ctx) {
+		if(ctx.INT()!=null) {
+s.setBitCnt(Integer.parseInt(ctx.INT().getText()));			
+		}else if(ctx.BI_N()!=null) {
+			s.setBitCntISN(true);
+		}
+		return s;
+	}
+
+	@Override
+	public HRACSymbol visitSymbol_dec(Symbol_decContext ctx) {
+	s=new HRACSymbol();
+	s.setBitCnt(1);
+	if(ctx.SYMBOL()!=null) {
+		s.setName(ctx.SYMBOL().getText());
+	}
+	if(ctx.cnt_specify()!=null) {
+		ctx.cnt_specify().accept(this);
+	}
+	if(ctx.symbol_os()!=null)
+	{
+		s.setTargetSymbol(ctx.symbol_os().accept(new MemoryAddressVisitor()));}
+	return s;
+	}
+
+
+
+}

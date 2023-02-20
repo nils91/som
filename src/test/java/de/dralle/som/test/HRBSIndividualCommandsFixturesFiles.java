e8b8f10b7d2a7d23fee0e5637f8d053cfe8ec156
Nils Dralle <nils.91@googlemail.com>
2023-02-20 16:26:02 +0100

    flippin commands

--- src/test/java/de/dralle/som/test/HRBSIndividualCommandsFixturesFiles.java
+++ src/test/java/de/dralle/som/test/HRBSIndividualCommandsFixturesFiles.java
@@ -169,7 +169,36 @@ class HRBSIndividualCommandsFixturesFiles {
 		return Stream.of(Arguments.of(false, false, false, true), Arguments.of(false, true, false, false),
 				Arguments.of(true, false, true, true), Arguments.of(true, true, true, false));
 	}
+	@ParameterizedTest
+	@MethodSource("provideTruthTableFLUIP1")
+	@Timeout(10)
+	void testFLIP1(boolean inValueAcc, boolean inValueA, boolean finalValueAcc, boolean finalValueA) throws IOException {
+		String hrbsCode = "import \"test/fixtures/hrbs/individual_commands/FLIP1.hrbs\"\n\nMAIN:\n\tglobal A\n\tDEBUG: FLIP1 A;";
+		HRBSModel hrbsModel = (HRBSModel) f.loadFromString(hrbsCode, SOMFormats.HRBS);
+		HRACModel hracModel = c.compile(hrbsModel, SOMFormats.HRBS, SOMFormats.HRAC);
+		HRASModel hrasModel = c.compile(hrbsModel, SOMFormats.HRBS, SOMFormats.HRAS);
+		IMemspace memspace = c.compile(hrbsModel, SOMFormats.HRBS, SOMFormats.BIN);
+		int aAdr = hrasModel.resolveSymbolToAddress("A");
+		int dbgAdr = hrasModel.resolveSymbolToAddress("MAIN_null_DEBUG");
+		SOMBitcodeRunner runner = new SOMBitcodeRunner((ISomMemspace) memspace);
+		runner.addDebugPoint(new AbstractCommandAddressListenerDP("DEBUG", dbgAdr) {
 
+			@Override
+			public boolean trigger(int cmdAddress, Opcode op, int tgtAddress, ISomMemspace memspace) {
+				memspace.setAccumulatorValue(inValueAcc);
+				memspace.setBit(aAdr, inValueA);
+				return true;
+			}
+		});
+		runner.execute();
+		assertTrue(runner.getMemspace().getAccumulatorValue() == finalValueAcc);
+		assertTrue(runner.getMemspace().getBit(aAdr) == finalValueA);
+	}
+
+	private static Stream<Arguments> provideTruthTableFLUIP1() {
+		return Stream.of(Arguments.of(false, false, false, false), Arguments.of(false, true, false, true),
+				Arguments.of(true, false, true, true), Arguments.of(true, true, true, false));
+	}
 	@ParameterizedTest
 	@MethodSource("provideTruthTableNOT1")
 	@Timeout(10)
@@ -722,6 +751,46 @@ class HRBSIndividualCommandsFixturesFiles {
 				Arguments.of(true, false, false, false, false, false),
 				Arguments.of(true, false, true, true, false, true), Arguments.of(true, true, false, true, true, false),
 				Arguments.of(true, true, true, false, true, true));
+	}
+	@ParameterizedTest
+	@MethodSource("provideTruthTableFLIP2")
+	@Timeout(10)
+	void testFLIP2(boolean inValueAcc, boolean inValueA, boolean inValueB, boolean finalValueAcc, boolean finalValueA,
+			boolean finalValueB) throws IOException {
+		String hrbsCode = "import \"test/fixtures/hrbs/individual_commands/FLIP2.hrbs\"\n\nMAIN:\n\tglobal A\n\tglobal B\n\tDEBUG: FLIP2 A,B;";
+		HRBSModel hrbsModel = (HRBSModel) f.loadFromString(hrbsCode, SOMFormats.HRBS);
+		HRACModel hracModel = c.compile(hrbsModel, SOMFormats.HRBS, SOMFormats.HRAC);
+		HRASModel hrasModel = c.compile(hrbsModel, SOMFormats.HRBS, SOMFormats.HRAS);
+		IMemspace memspace = c.compile(hrbsModel, SOMFormats.HRBS, SOMFormats.BIN);
+		int aAdr = hrasModel.resolveSymbolToAddress("A");
+		int bAdr = hrasModel.resolveSymbolToAddress("B");
+		int dbgAdr = hrasModel.resolveSymbolToAddress("MAIN_null_DEBUG");
+		SOMBitcodeRunner runner = new SOMBitcodeRunner((ISomMemspace) memspace);
+		runner.addDebugPoint(new AbstractCommandAddressListenerDP("DEBUG", dbgAdr) {
+
+			@Override
+			public boolean trigger(int cmdAddress, Opcode op, int tgtAddress, ISomMemspace memspace) {
+				memspace.setAccumulatorValue(inValueAcc);
+				memspace.setBit(aAdr, inValueA);
+				memspace.setBit(bAdr, inValueB);
+				return true;
+			}
+		});
+		runner.execute();
+		assertEquals(finalValueAcc, runner.getMemspace().getAccumulatorValue());
+		assertEquals(finalValueA, runner.getMemspace().getBit(aAdr));
+		assertEquals(finalValueB, runner.getMemspace().getBit(bAdr));
+	}
+
+	private static Stream<Arguments> provideTruthTableFLIP2() {
+		return Stream.of(Arguments.of(false, false, false, false, false, false),
+				Arguments.of(false, false, true, false, false, true),
+				Arguments.of(false, true, false, false, true, true), 
+				Arguments.of(false, true, true, false, true, false),
+				Arguments.of(true, false, false, true, false, false),
+				Arguments.of(true, false, true, true, false, true), 
+				Arguments.of(true, true, false, true, true, true),
+				Arguments.of(true, true, true, true, true, false));
 	}
 
 	@ParameterizedTest

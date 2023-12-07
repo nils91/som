ac143dc0b0d7d8c821ad30f3e578aad88bb0db86
Nils Dralle <nils.91@googlemail.com>
2023-12-07 14:00:00 +0100

    implemented new hrbs model class

--- src/main/java/de/dralle/som/languages/hrbs/model/HRBSFixedMemoryAddress.java
+++ src/main/java/de/dralle/som/languages/hrbs/model/HRBSFixedMemoryAddress.java
@@ -1,7 +1,38 @@
 package de.dralle.som.languages.hrbs.model;
 
-public class HRBSFixedMemoryAddress extends HRBSMemoryAddress {
+public class HRBSFixedMemoryAddress extends AbstractHRBSMemoryAddress {
+	@Override
+	public int hashCode() {
+		return super.hashCode()+address;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof HRBSFixedMemoryAddress) {
+			return address==((HRBSFixedMemoryAddress)obj).address&&super.equals(obj);
+		}
+		return super.equals(obj);
+	}
+
+	@Override
+	public HRBSFixedMemoryAddress clone() {
+		// TODO Auto-generated method stub
+		HRBSFixedMemoryAddress clone= (HRBSFixedMemoryAddress) super.clone();
+		clone.address=address;
+		return clone;
+	}
+
+	@Override
+	public String asHRBSCode() {
+		return getFirstPartHRBSCode()+"@"+address+getSecondPartHRBSCode();
+	}
+
 	private int address;
+
+	public HRBSFixedMemoryAddress(int address) {
+		super();
+		this.address = address;
+	}
 
 	public int getAddress() {
 		return address;

c891f2f84662968d5d3e9fd68867b0d9625ac8c4
Nils Dralle <nils.91@googlemail.com>
2023-12-07 13:50:35 +0100

    new model class for hrbs

---
+++ src/main/java/de/dralle/som/languages/hrbs/model/HRBSFixedMemoryAddress.java
@@ -0,0 +1,13 @@
+package de.dralle.som.languages.hrbs.model;
+
+public class HRBSFixedMemoryAddress extends HRBSMemoryAddress {
+	private int address;
+
+	public int getAddress() {
+		return address;
+	}
+
+	public void setAddress(int address) {
+		this.address = address;
+	}
+}

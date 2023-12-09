2b2d642acf6324f084a7a04e891c12f245fba16c
Nils Dralle <nils.91@googlemail.com>
2023-12-09 15:03:15 +0100

    additional tests for #62

--- src/test/java/de/dralle/som/test/IssueTests.java
+++ src/test/java/de/dralle/som/test/IssueTests.java
@@ -3,6 +3,7 @@ package de.dralle.som.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import de.dralle.som.Compiler;
 import de.dralle.som.FileLoader;
 import de.dralle.som.SOMFormats;
+import de.dralle.som.languages.hrac.model.HRACForDup;
 import de.dralle.som.languages.hrac.model.HRACModel;
 import de.dralle.som.languages.hras.model.HRASModel;
 
@@ -45,5 +47,29 @@ class IssueTests {
 		HRACModel hrap = c.compile(model, SOMFormats.HRAC, SOMFormats.HRAP);
 		assertNotNull(hrap);
 	}
-
+	@Test
+	void testIssue62_HRAPCompilationIsPrecompiled() throws IOException {
+		HRACModel model = f.loadFromFile("test/fixtures/hrac/test_for_simple.hrac", SOMFormats.HRAC);
+		HRACModel hrap = c.compile(model, SOMFormats.HRAC, SOMFormats.HRAP);
+		List<HRACForDup> cs = hrap.getCommands();
+		boolean pc=true;
+		for (HRACForDup hracForDup : cs) {
+			if(hracForDup.getCmd()==null) {
+				pc = false;
+			}
+		}
+		assertTrue(pc);
+	}
+	@Test
+	void testIssue62_HRAPCompilationOriginalModelNeedsPrecompilation() throws IOException {
+		HRACModel model = f.loadFromFile("test/fixtures/hrac/test_for_simple.hrac", SOMFormats.HRAC);
+		List<HRACForDup> cs = model.getCommands();
+		boolean pc=true;
+		for (HRACForDup hracForDup : cs) {
+			if(hracForDup.getCmd()==null) {
+				pc = false;
+			}
+		}
+		assertFalse(pc);
+	}
 }

e9770ca2f3cb00ecdb82ffd1ebce1599b08b4d5b
Nils Dralle <nils.91@googlemail.com>
2024-04-23 15:20:54 +0200

    updated range generation in HRAC

--- src/main/java/de/dralle/som/languages/hrac/model/HRACForDupRange.java
+++ src/main/java/de/dralle/som/languages/hrac/model/HRACForDupRange.java
@@ -3,8 +3,46 @@ package de.dralle.som.languages.hrac.model;
 public class HRACForDupRange implements Cloneable {
 	private int rangeStart;
 	private int rangeEnd;
+	private int stepSize=1;
 	private String rangeStartSpecial;
 	private String rangeEndSpecial;
+	private String stepSizeSpecial;
+	private boolean lowerBoundExclusive = false;
+	private boolean upperBoundExclusive = false
+
+	;
+
+	public int getStepSize() {
+		return stepSize;
+	}
+
+	public boolean isLowerBoundExclusive() {
+		return lowerBoundExclusive;
+	}
+
+	public void setLowerBoundExclusive(boolean lowerBoundExclusive) {
+		this.lowerBoundExclusive = lowerBoundExclusive;
+	}
+
+	public boolean isUpperBoundExclusive() {
+		return upperBoundExclusive;
+	}
+
+	public void setUpperBoundExclusive(boolean upperBoundExclusive) {
+		this.upperBoundExclusive = upperBoundExclusive;
+	}
+
+	public void setStepSize(int stepSize) {
+		this.stepSize = stepSize;
+	}
+
+	public String getStepSizeSpecial() {
+		return stepSizeSpecial;
+	}
+
+	public void setStepSizeSpecial(String stepSizeSpecial) {
+		this.stepSizeSpecial = stepSizeSpecial;
+	}
 
 	public int getRangeStart() {
 		return rangeStart;
@@ -45,23 +83,63 @@ public class HRACForDupRange implements Cloneable {
 		if (rangeStartSpecial != null) {
 			rangeStart = parent.getDirectiveAsInt(rangeStartSpecial);
 		}
+		if (stepSizeSpecial != null) {
+			stepSize = parent.getDirectiveAsInt(stepSizeSpecial);
+		}
 		int[] rng = null;
 		if (rangeStart <= rangeEnd) {
-			rng = new int[rangeEnd - rangeStart + 1];
-			for (int i = 0; i < rng.length; i++) {
-				rng[i] = rangeStart + i;
+			if (!lowerBoundExclusive && !upperBoundExclusive) {
+				rng = new int[(rangeEnd - rangeStart - 1) / stepSize];
+				for (int i = 0; i < rng.length; i ++) {
+					rng[i] = rangeStart + i*stepSize + 1;
+				}
+			} else if (lowerBoundExclusive && upperBoundExclusive) {
+				rng = new int[(rangeEnd - rangeStart + 1) / stepSize];
+				for (int i = 0; i < rng.length; i ++) {
+					rng[i] = rangeStart + i*stepSize;
+				}
+			} else if (lowerBoundExclusive) {
+				rng = new int[(rangeEnd - rangeStart) / stepSize];
+				for (int i = 0; i < rng.length; i ++) {
+					rng[i] = rangeStart + i*stepSize+1;
+				}
+			} else if (upperBoundExclusive) {
+				rng = new int[(rangeEnd - rangeStart) / stepSize];
+				for (int i = 0; i < rng.length; i ++) {
+					rng[i] = rangeStart + i*stepSize;
+				}
 			}
+
 		} else {
-			rng = new int[rangeStart - rangeEnd + 1];
-			for (int i = 0; i < rng.length; i++) {
-				rng[i] = rangeStart - i;
+			if (!lowerBoundExclusive && !upperBoundExclusive) {
+				rng = new int[(rangeStart - rangeEnd - 1) / stepSize];
+				for (int i = 0; i < rng.length; i ++) {
+					rng[i] = rangeStart - i*stepSize + 1;
+				}
+			} else if (lowerBoundExclusive && upperBoundExclusive) {
+				rng = new int[(rangeStart - rangeEnd + 1) / stepSize];
+				for (int i = 0; i < rng.length; i ++) {
+					rng[i] = rangeStart - i*stepSize;
+				}
+			} else if (lowerBoundExclusive) {
+				rng = new int[(rangeStart - rangeEnd) / stepSize];
+				for (int i = 0; i < rng.length; i ++) {
+					rng[i] = rangeStart - i*stepSize+1;
+				}
+			} else if (upperBoundExclusive) {
+				rng = new int[(rangeStart - rangeEnd) / stepSize];
+				for (int i = 0; i < rng.length; i ++) {
+					rng[i] = rangeStart - i*stepSize;
+				}
 			}
+
+			
 		}
 		return rng;
 	}
 
 	@Override
-	public HRACForDupRange clone(){
+	public HRACForDupRange clone() {
 		// TODO Auto-generated method stub
 		try {
 			return (HRACForDupRange) super.clone();

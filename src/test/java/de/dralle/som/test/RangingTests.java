af6e81126f9a8522bb8063c77e1ccdb8448b84fa
Nils Dralle <nils.91@googlemail.com>
2024-04-25 14:52:51 +0200

    new test for ranges

---
+++ src/test/java/de/dralle/som/test/RangingTests.java
@@ -0,0 +1,66 @@
+package de.dralle.som.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import de.dralle.som.AbstractCommandAddressListenerDP;
+import de.dralle.som.Compiler;
+import de.dralle.som.IMemspace;
+import de.dralle.som.ISomMemspace;
+import de.dralle.som.Opcode;
+import de.dralle.som.SOMBitcodeRunner;
+import de.dralle.som.SOMFormats;
+import de.dralle.som.languages.hrac.model.HRACForDup;
+import de.dralle.som.languages.hrac.model.HRACForDupRange;
+import de.dralle.som.languages.hrac.model.HRACModel;
+import de.dralle.som.languages.hras.model.HRASModel;
+import de.dralle.som.languages.hrbs.model.HRBSModel;
+
+class RangingTests {
+
+	private HRACForDupRange hracForDup;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		hracForDup = new HRACForDupRange();
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideRangeTestData")
+	void hracRangeGen(int lower, int upper, int step, boolean lowerExclusive, boolean upperExclusive, int[] expected)
+			throws IOException {
+		hracForDup.setRangeStart(lower);
+		hracForDup.setRangeEnd(upper);
+		hracForDup.setStepSize(step);
+		hracForDup.setLowerBoundExclusive(lowerExclusive);
+		hracForDup.setUpperBoundExclusive(upperExclusive);
+		assertArrayEquals(expected, hracForDup.getRange(null));
+	}
+
+	private static Stream<Arguments> provideRangeTestData() {// lower bound, upper bound, step, lower exclusive, upper
+																// exclusive, expected
+		return Stream.of(Arguments.of(1, 2, 1, false, false, new int[] { 1, 2 }),
+				Arguments.of(1, 2, 1, true, false, new int[] { 2 }),
+				Arguments.of(1, 2, 1, false, true, new int[] { 1 }), Arguments.of(1, 2, 1, true, true, new int[] {}),
+				Arguments.of(1, 2, 2, false, false, new int[] { 1 }),
+				Arguments.of(1, 2, 2, true, false, new int[] { 2 }),
+				Arguments.of(1, 2, 2, false, true, new int[] { 1 }), Arguments.of(1, 2, 2, true, true, new int[] {}),
+				Arguments.of(1, 2, 3, false, false, new int[] { 1 }),
+				Arguments.of(1, 2, 3, true, false, new int[] { 2 }),
+				Arguments.of(1, 2, 3, false, true, new int[] { 1 }), Arguments.of(1, 2, 3, true, true, new int[] {}),
+
+				Arguments.of(1, 2, 2, false, false, new int[] { 1 }),
+				Arguments.of(1, 2, 2, false, false, new int[] { 1 }),
+				Arguments.of(1, 2, 2, false, false, new int[] { 1 }));
+	}
+
+}

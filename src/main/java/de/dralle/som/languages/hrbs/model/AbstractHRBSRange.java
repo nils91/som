6aae8bbef785bb8ca55786e9b5fd318d2e40dfe6
Nils Dralle <nils.91@googlemail.com>
2024-05-20 17:24:07 +0200

    abstract range masterclass

---
+++ src/main/java/de/dralle/som/languages/hrbs/model/AbstractHRBSRange.java
@@ -0,0 +1,54 @@
+package de.dralle.som.languages.hrbs.model;
+
+public class AbstractHRBSRange implements Cloneable {
+	private String runningDirectiveName;
+
+	@Override
+	public int hashCode() {
+		return runningDirectiveName.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof AbstractHRBSRange) {
+			AbstractHRBSRange oth = (AbstractHRBSRange) obj;
+			if (runningDirectiveName != null) {
+				return runningDirectiveName.equals(oth.runningDirectiveName);
+			}
+		}
+		return super.equals(obj);
+	}
+
+	@Override
+	public Object clone() {
+		// TODO Auto-generated method stub
+		try {
+			return super.clone();
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return runningDirectiveName;
+	}
+
+	@Override
+	public String toString() {
+		String str = "";
+		if(runningDirectiveName!=null) {
+			str+="$"+runningDirectiveName+" = ";
+		}
+		return str;
+	}
+
+	public String getRunningDirectiveName() {
+		if (runningDirectiveName == null) {
+			return "i";// return default if not set otherwise
+		}
+		return runningDirectiveName;
+	}
+
+	public void setRunningDirectiveName(String runningDirectiveName) {
+		this.runningDirectiveName = runningDirectiveName;
+	}
+
+}

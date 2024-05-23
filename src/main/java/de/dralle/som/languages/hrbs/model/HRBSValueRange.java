21099fb27c95a10dabce924ef7027f6c45925dd8
Nils Dralle <nils.91@googlemail.com>
2024-05-23 15:00:31 +0200

    implemented equals, clone and toString valuerange

--- src/main/java/de/dralle/som/languages/hrbs/model/HRBSValueRange.java
+++ src/main/java/de/dralle/som/languages/hrbs/model/HRBSValueRange.java
@@ -3,9 +3,10 @@ package de.dralle.som.languages.hrbs.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HRBSValueRange extends AbstractHRBSRange implements Cloneable{
+public class HRBSValueRange extends AbstractHRBSRange implements Cloneable {
 
-	private List<HRBSMemoryAddressOffset> values=new ArrayList<HRBSMemoryAddressOffset>();
+	private List<HRBSMemoryAddressOffset> values = new ArrayList<HRBSMemoryAddressOffset>();
+
 	public List<HRBSMemoryAddressOffset> getValues() {
 		return values;
 	}
@@ -13,6 +14,7 @@ public class HRBSValueRange extends AbstractHRBSRange implements Cloneable{
 	public void setValues(List<HRBSMemoryAddressOffset> values) {
 		this.values = values;
 	}
+
 	public void addValue(HRBSMemoryAddressOffset value) {
 		values.add(value);
 	}
@@ -22,28 +24,50 @@ public class HRBSValueRange extends AbstractHRBSRange implements Cloneable{
 		int hc = super.hashCode();
 		for (int i = 0; i < values.size(); i++) {
 			HRBSMemoryAddressOffset array_element = values.get(i);
-			hc+=array_element.hashCode()*(i+1);
-			
+			hc += array_element.hashCode() * (i + 1);
+
 		}
 		return hc;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
-		return super.equals(obj);
+		if (obj instanceof HRBSValueRange) {
+			HRBSValueRange oth = (HRBSValueRange) obj;
+			boolean eq = super.equals(obj);
+			eq = eq && values.size() == oth.values.size();
+			if (eq) {
+				for (int i = 0; i < values.size(); i++) {
+					HRBSMemoryAddressOffset array_element = values.get(i);
+					eq = eq && array_element.equals(oth.values.get(i));
+
+				}
+				return eq;
+			}
+
+		}
+		return false;
 	}
 
 	@Override
-	public AbstractHRBSRange clone() {
-		// TODO Auto-generated method stub
-		return super.clone();
+	public HRBSValueRange clone() {
+		HRBSValueRange cl=(HRBSValueRange) super.clone();
+		cl.values=new ArrayList<>();
+		for (HRBSMemoryAddressOffset hrbsMemoryAddressOffset : values) {
+			cl.values.add(hrbsMemoryAddressOffset.clone());
+		}
+		return cl;
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
+		String str = super.toString();
+		str+="{";
+		for (HRBSMemoryAddressOffset hrbsMemoryAddressOffset : values) {
+			str+=hrbsMemoryAddressOffset.toString()+", ";
+		}
+		str=str.substring(0, str.length()-2)+"}";
+		return str;
 	}
 
 }

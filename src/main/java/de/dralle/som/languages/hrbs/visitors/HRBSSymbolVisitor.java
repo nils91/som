712e8a5eb23d97effed9d9730d5589c066e51664
Nils Dralle <nils.91@googlemail.com>
2025-01-04 15:22:23 +0100

    visitor changes WIP

--- src/main/java/de/dralle/som/languages/hrbs/visitors/HRBSSymbolVisitor.java
+++ src/main/java/de/dralle/som/languages/hrbs/visitors/HRBSSymbolVisitor.java
@@ -35,7 +35,7 @@ public class HRBSSymbolVisitor extends HRBSGrammarBaseVisitor<HRBSSymbol> {
 	@Override
 	public HRBSSymbol visitCnt_specify(Cnt_specifyContext ctx) {
 		if (ctx.primary_expr() != null) {
-			s.setBitCnt((ctx.primary_expr().accept(new HRBSExpressionVisitor()).compileToHRAC().calculateNumericalValue()));
+			s.setBitCnt((ctx.primary_expr().accept(new HRBSExpressionVisitor());
 		}
 		if(ctx.directive_access()!=null) {
 			ctx.directive_access().accept(this);

50fe6b0c266da1ff8e5c053acb178fbd50dd6624
Nils Dralle <nils.91@googlemail.com>
2025-01-05 16:22:04 +0100

    no dedicated offset  class anymore

--- src/main/java/de/dralle/som/languages/hrbs/visitors/HRBSMemoryAddressOffsetSpecifyVisitor.java
+++ src/main/java/de/dralle/som/languages/hrbs/visitors/HRBSMemoryAddressOffsetSpecifyVisitor.java
@@ -8,8 +8,9 @@ import de.dralle.som.languages.hrbs.generated.HRBSGrammarParser;
 import de.dralle.som.languages.hrbs.generated.HRBSGrammarParser.Directive_accessContext;
 import de.dralle.som.languages.hrbs.generated.HRBSGrammarParser.Offset_specify_numberContext;
 import de.dralle.som.languages.hrbs.model.AbstractHRBSMemoryAddress;
-import de.dralle.som.languages.hrbs.model.HRBSMemoryAddressOffset;
 import de.dralle.som.languages.hrbs.model.HRBSSymbol;
+import de.dralle.som.languages.hrbs.model.expressiontree.HRBSAbstractExpressionNode;
+import de.dralle.som.languages.hrbs.model.expressiontree.HRBSDirectiveNode;
 import de.dralle.som.Util;
 import de.dralle.som.languages.hras.generated.HRASGrammarBaseVisitor;
 import de.dralle.som.languages.hras.generated.HRASGrammarParser.Int_or_symbolContext;
@@ -20,35 +21,32 @@ import de.dralle.som.languages.hras.model.SymbolHRASMemoryAddress;
  * @author Nils
  *
  */
-public class HRBSMemoryAddressOffsetSpecifyVisitor extends HRBSGrammarBaseVisitor<HRBSMemoryAddressOffset> {
+public class HRBSMemoryAddressOffsetSpecifyVisitor extends HRBSGrammarBaseVisitor<HRBSAbstractExpressionNode> {
 
-	private HRBSMemoryAddressOffset o;
+	private HRBSAbstractExpressionNode o;
 
-	public HRBSMemoryAddressOffsetSpecifyVisitor() {
-		o=new HRBSMemoryAddressOffset();
-	}
-	public HRBSMemoryAddressOffsetSpecifyVisitor(HRBSMemoryAddressOffset o) {
+	public HRBSMemoryAddressOffsetSpecifyVisitor(HRBSAbstractExpressionNode o) {
 		this.o=o;
 	}
 
 	@Override
-	public HRBSMemoryAddressOffset visitOffset_specify(HRBSGrammarParser.Offset_specifyContext ctx) {
+	public HRBSAbstractExpressionNode visitOffset_specify(HRBSGrammarParser.Offset_specifyContext ctx) {
 		ctx.offset_specify_number().accept(this);
 		return o;
 	}
 	@Override
-	public HRBSMemoryAddressOffset visitOffset_specify_number(Offset_specify_numberContext ctx) {
+	public HRBSAbstractExpressionNode visitOffset_specify_number(Offset_specify_numberContext ctx) {
 		if(ctx.primary_expr()!=null) {
-			o.setOffset((ctx.primary_expr().accept(new HRBSExpressionVisitor()).compileToHRAC().calculateNumericalValue()));
+			o=((ctx.primary_expr().accept(new HRBSExpressionVisitor())));
 		}
 		if(ctx.directive_access()!=null) {
-			ctx.directive_access().accept(this);
+			o=ctx.directive_access().accept(this);
 		}
 		return o;
 	}
 	@Override
-	public HRBSMemoryAddressOffset visitDirective_access(Directive_accessContext ctx) {
-		o.setDirectiveAccessName(ctx.directive_name().getText());
+	public HRBSAbstractExpressionNode visitDirective_access(Directive_accessContext ctx) {
+		o=new HRBSDirectiveNode(ctx.directive_name().getText());
 		return o;
 	}
 

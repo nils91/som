73fac08f5e830ca09dcbe9775a31e749695bbf16
Nils Dralle <nils.91@googlemail.com>
2025-01-05 15:52:20 +0100

    finally remove directive name

--- src/main/java/de/dralle/som/languages/hrac/model/HRACMemoryOffset.java
+++ src/main/java/de/dralle/som/languages/hrac/model/HRACMemoryOffset.java
@@ -8,11 +8,6 @@ private HRACAbstractExpressionNode offset;
 public void setOffset(HRACAbstractExpressionNode offset) {
 	this.offset = offset;
 }
-/**
- * This functionality is now handled by offset - can be directive node
- */
-@Deprecated
-private String directiveName;
 public HRACMemoryOffset() {
 	
 }
@@ -24,13 +19,5 @@ public HRACAbstractExpressionNode getOffset() {
 }
 public void setOffset(int offset) {
 	this.offset = new HRACIntegerNode(offset);
-}
-@Deprecated
-public String getDirectiveName() {
-	return directiveName;
-}
-@Deprecated
-public void setDirectiveName(String directiveName) {
-	this.directiveName = directiveName;
 }
 }

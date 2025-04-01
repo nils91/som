7d8f8c932a02978491527cd27127bd255eac9ebf
Nils Dralle <nils.91@googlemail.com>
2025-04-01 16:23:07 +0200

    hras -> hrav et resolve test

--- src/test/java/de/dralle/som/test/HRASLanguageFeaturesTests.java
+++ src/test/java/de/dralle/som/test/HRASLanguageFeaturesTests.java
@@ -5,9 +5,13 @@ package de.dralle.som.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -19,7 +23,18 @@ import de.dralle.som.Compiler;
 import de.dralle.som.FileLoader;
 import de.dralle.som.IMemspace;
 import de.dralle.som.SOMFormats;
+import de.dralle.som.languages.hrac.model.AbstractHRACMemoryAddress;
+import de.dralle.som.languages.hrac.model.FixedHRACMemoryAddress;
+import de.dralle.som.languages.hrac.model.HRACForDup;
+import de.dralle.som.languages.hrac.model.HRACModel;
+import de.dralle.som.languages.hrac.model.HRACSymbol;
+import de.dralle.som.languages.hrac.model.expressiontree.HRACAbstractExpressionNode;
+import de.dralle.som.languages.hrac.model.expressiontree.HRACDirectiveNode;
+import de.dralle.som.languages.hrac.model.expressiontree.HRACMultiplicationExpressionNode;
 import de.dralle.som.languages.hras.model.HRASModel;
+import de.dralle.som.languages.hrav.model.HRAVCommand;
+import de.dralle.som.languages.hrav.model.HRAVModel;
+import de.dralle.som.languages.hrbs.model.HRBSModel;
 
 /**
  * @author Nils
@@ -102,5 +117,24 @@ class HRASLanguageFeaturesTests {
 		assertEquals(19, model.resolveSymbolToAddress("B"));
 		assertEquals(19, model.resolveSymbolToAddress("C"));
 		assertEquals(19, model.resolveSymbolToAddress("D"));
+	}
+	
+	@Test
+	void testExpressionTreeResolveWhenCompiling2HRAV() throws IOException {
+		HRASModel model = f.loadFromFile("test/fixtures/hras/features/expression-tree/test_et_correct_compile.hras", SOMFormats.HRAS);
+		HRAVModel hrac = c.compile(model, SOMFormats.HRAS, SOMFormats.HRAV);
+		Map<Integer, HRAVCommand> hracC = hrac.getCommands();
+		HRAVCommand foundCMD = null;
+		//the command this test is looking for has 11*13 as its target address in HRAS, which should get resolved to 143 in HRAV
+		for (Entry<Integer, HRAVCommand> entry : hracC.entrySet()) {
+			Integer key = entry.getKey();
+			HRAVCommand val = entry.getValue();
+			if(val.getAddress()==143) {
+				foundCMD=val;
+			}
+		}
+		assertNotNull(foundCMD);
+		assertEquals(143, foundCMD.getAddress());
+	
 	}
 }

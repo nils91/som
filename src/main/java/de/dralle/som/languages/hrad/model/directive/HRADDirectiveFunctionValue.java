54826279de00aedee4c5ae564ee3c5f31b421c8b
Nils Dralle <nils.91@googlemail.com>
2026-04-12 16:55:33 +0200

    HRADDirectiveFunctionValue

--- src/main/java/de/dralle/som/languages/hrad/model/directive/HRADDirectiveFunctionValue.java
+++ src/main/java/de/dralle/som/languages/hrad/model/directive/HRADDirectiveFunctionValue.java
@@ -1,12 +1,15 @@
 package de.dralle.som.languages.hrad.model.directive;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import de.dralle.som.languages.hrad.HRADSourceLocation;
 import de.dralle.som.languages.hrad.model.expressiontree.HRADIntegerNode;
 
 public class HRADDirectiveFunctionValue extends HRADAbstractDirectiveValue<HRADAbstractDirectiveValue<?>> {
 	
-	private String[] params;
+	private Map<String,String> params=new LinkedHashMap<String, String>();
 
-	public HRADDirectiveFunctionValue( HRADAbstractDirectiveValue<?> value,String[] params, HRADSourceLocation sourceLocation) {
+	public HRADDirectiveFunctionValue( HRADAbstractDirectiveValue<?> value,Map<String, String> params, HRADSourceLocation sourceLocation) {
 		super(value, sourceLocation);
 		this.params=params;
 	}
